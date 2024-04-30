28066d698d8248fa6e494879e74df4bfd23445d5
gary2245 <justinnguyen2245@gmail.com>
2024-04-30 00:49:54 -0700

    Player movement, rotation, death animation
    Added player movement, Added player rotation based on cursor location, Added death animation (not fully implemented yet)

---
+++ CS4700_FinalProject/Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtMouse();
+    }
+
+    private void LookAtMouse()
+    {
+        //first gets the location of where the mouse is and subtracts it from player's location to get the cursor's location relative to the player
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - playerTransform.position;
+        //using that location (which is a vector), it calculates the angle of how much the player need to rotate
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        
+        //creates a quaternion with a rotation in the z-axis, the angle-90 is to account for the default sprite which faces up
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        playerTransform.rotation = rotation;
+    }
+}
---
+++ CS4700_FinalProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+
+
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    //called every fixed framerate frame, physics stuff done here
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveSpeed * moveDirection.x, moveSpeed * moveDirection.y);
+    }
+}
