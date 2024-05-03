eac769a33fa0f58fb24553ddf21f6d8d4dbe4873
gary2245 <justinnguyen2245@gmail.com>
2024-05-02 23:43:24 -0700

    Add hold to shoot and animation
    Added shooting animation
    Added holding mouse to shoot

--- CS4700_FinalProject/Assets/Scripts/Player/Weapon.cs
+++ CS4700_FinalProject/Assets/Scripts/Player/Weapon.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-
+    public float fireRate; //expressed in bullets per second
     public Transform firePoint;
     public GameObject bulletPrefab;
+
+    private float nextFireTime;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        // shoot only at a certain firerate and when left click pressed
+        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
         {
             Shoot();
+            nextFireTime = Time.time + 1f / fireRate;
         }
     }
 
-        void Shoot()
+    void Shoot()
     {
+        animator.SetTrigger("playerShooting");
         // Instantiate a new bullet
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-
     }
 }
