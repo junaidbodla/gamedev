876b1bc902c2bf8f857a8898ac9fae48203ee1fe
Megan <mjoo@cpp.edu>
2024-05-03 16:28:25 -0700

    add timer for enemy spawn

--- CS4700_FinalProject/Assets/Scripts/Enemies/EnemySpawner.cs
+++ CS4700_FinalProject/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -13,11 +15,19 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private float _maximumSpawnTime;
 
+    [SerializeField]
+     private float _spawnDuration;
+
     private float _timeUntilSpawn;
+    private float _spawnTimer;
+    public Slider timer;
 
     void Awake()
     {
         SetTimeUntilSpawn();
+	//Set timer and slider to whatever spawn duration
+	timer.maxValue = _spawnDuration;
+	_spawnTimer = _spawnDuration;
     }
 
     // Update is called once per frame
@@ -30,6 +40,17 @@ public class EnemySpawner : MonoBehaviour
             Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
             SetTimeUntilSpawn();
         }
+
+	//once spawn timer is at 0 stop spawning
+	_spawnTimer -= Time.deltaTime;
+	if (_spawnTimer <= 0 ) {
+		enabled = false;
+	}
+
+	//slider to show time
+	if (timer != null) {
+		timer.value = _spawnTimer;
+	}
     }
 
     private void SetTimeUntilSpawn()
